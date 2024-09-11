741bdc0660a883957c560abf46b0f1a0fc07e7a5
Akrom <akromxolboyev0615@gmail.com>
2024-09-11 16:44:35 +0500

    .

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="bg-red-500 container mx-auto">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et expedita
+      nesciunt, quibusdam, eum assumenda cum itaque fugit recusandae quod sunt
+      voluptas accusamus, placeat fuga. Id fugit aliquid deserunt laudantium ad.
+    </div>
+  );
+}
